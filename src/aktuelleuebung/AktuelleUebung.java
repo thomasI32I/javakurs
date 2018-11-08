ec83c965f6a6400355d12a07937b9e5c16d2e167
Thomas Rose <mail@tom-rose.de>
2018-11-08 15:33:21 +0100

    Addition nur mit bitweisen Operatoren und Subtraktion

--- src/aktuelleuebung/AktuelleUebung.java
+++ src/aktuelleuebung/AktuelleUebung.java
@@ -5,22 +5,19 @@ public class AktuelleUebung
 
     public static void main( String[] args )
     {
-        System.out.println( bitweise_addidition( 2, -5 ) );
-
+        System.out.println( bitweise_addidition( 78, 1 ) );
     }
 
     private static int bitweise_addidition( int a, int b )
     {
         while ( b != 0 )
         {
-            int uebertrag = a & b;
-
+            int uebertrag = a & b; // wenn Übertrag negiert wird ~, dann Subtraktion
             a = a ^ b;
             b = uebertrag << 1;
+            
         }
-
         return a;
-
     }
 
 }

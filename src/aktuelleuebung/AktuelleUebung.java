8f6c544fc0b8e569d59787ed79c934ba0308da45
Thomas Rose <mail@tom-rose.de>
2018-11-09 14:34:27 +0100

    Umwandeln von Dezimalzahlen in Binär- und Hexadezimalzahlen

--- src/aktuelleuebung/AktuelleUebung.java
+++ src/aktuelleuebung/AktuelleUebung.java
@@ -1,23 +1,58 @@
 package aktuelleuebung;
 
+import java.util.Scanner;
+
 public class AktuelleUebung
 {
 
     public static void main( String[] args )
     {
-        System.out.println( bitweise_addidition( 78, 1 ) );
+        System.out.print( "Bitte geben Sie eine Zahl: " );
+        Scanner sc = new Scanner( System.in );
+        int i = sc.nextInt();
+
+        System.out.println( hexadezimal( i ) );
+        System.out.println( Integer.toHexString( i ) );
+
+        System.out.println( binaer( i ) );
+        System.out.println( Integer.toBinaryString( i ) );
     }
 
-    private static int bitweise_addidition( int a, int b )
+    private static String binaer( int i )
     {
-        while ( b != 0 )
+        String zahl = "", ziffer = "";
+        boolean firstNonZero = false;
+
+        int gt = 4096;
+        while ( gt > 0 )
         {
-            int uebertrag = a & b; // wenn Übertrag negiert wird ~, dann Subtraktion
-            a = a ^ b;
-            b = uebertrag << 1;
-            
+            ziffer = Integer.toString( i / gt );
+
+            i -= gt * ( i / gt );
+            gt /= 2;
+
+            if ( ziffer.equals( "1" ) )
+                firstNonZero = true;
+            if ( firstNonZero )
+                zahl += ziffer;
         }
-        return a;
+        return zahl;
+
+    }
+
+    private static String hexadezimal( int i )
+    {
+        String zahl = "";
+        char[] hexZiffern = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int gt = 4096;
+        while ( gt > 0 )
+        {           
+            zahl += hexZiffern[ i / gt ];
+            i -= gt * ( i / gt );
+            gt /= 16;     
+        }
+        return zahl;
+
     }
 
 }
